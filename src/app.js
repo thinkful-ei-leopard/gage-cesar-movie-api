c319739f355772bb7970faa187c3db85e6583af9
gage117 <57643501+gage117@users.noreply.github.com>
2020-02-19 14:25:51 -0600

    first tests working

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use(function validateToken(req, res, next){
-  const apiToken = process.env.API_TOKEN
+  const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
   if(!authToken || authToken.split(' ')[1] !== apiToken){
     return res.status(401).json({ error: 'Unauthorized request'});
@@ -21,16 +21,27 @@ app.use(function validateToken(req, res, next){
 });
 
 app.get('/', (req, res) => {
-  res.send("Hey, you're not supposed to be here, get the **** outta here! Get onto /movie!");
+  res.send('Hey, you\'re not supposed to be here, get the **** outta here! Get onto /movie!');
 });
 
 function handleGetMovie(req, res) {
   let response = [...movieList];
 
   if (req.query.genre) {
+    let validGenres = [];
+    response.map(movie => {
+      if (!validGenres.includes(movie.genre)) {
+        validGenres.push(movie.genre);
+      }
+    });
+    if (!validGenres.includes(req.query.genre)) {
+      res
+        .status(400)
+        .send('The genre you queried is invalid')
+    }
     response = response.filter(movie =>
       movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-    )
+    );
   }
   if (req.query.country) {
     response = response.filter(movie => 
--- test/app.test.js
+++ test/app.test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const expect = require('chai').expect;
 const supertest = require('supertest');
 const app = require('../src/app');
@@ -8,18 +9,20 @@ describe('App module', () => {
     it('should return 200 and JSON data of movies', () => {
       return supertest(app)
         .get('/movie')
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
         .expect(200)
         .expect('Content-Type', /json/)
         .then(res => {
-          expect((res.body).to.be.an('array'));
+          expect(res.body).to.be.an('array');
         });
     });
 
     it('should return 400 when genre param invalid', () => {
       return supertest(app)
-        .get('movie')
+        .get('/movie')
+        .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
         .query({ genre: 'invalid'})
-        .expect(400, 'Queried genre invalid');
+        .expect(400, 'The genre you queried is invalid');
     });
   });
 });
